e4fc096de2d98b702ced12d93a6c9a85c45dd4c6
Don-Smilo <don_smilo@mail.ru>
2023-10-14 17:32:40 +0300

    edited character moving
    jump power and ari control

--- Source/Platformer/PlatformerCharacter.cpp
+++ Source/Platformer/PlatformerCharacter.cpp
@@ -30,8 +30,8 @@ APlatformerCharacter::APlatformerCharacter()
 
 	// Note: For faster iteration times these variables, and many more, can be tweaked in the Character Blueprint
 	// instead of recompiling to adjust them
-	GetCharacterMovement()->JumpZVelocity = 700.f;
-	GetCharacterMovement()->AirControl = 0.35f;
+	GetCharacterMovement()->JumpZVelocity = 1500.f;
+	GetCharacterMovement()->AirControl = 0.8f;
 	GetCharacterMovement()->MaxWalkSpeed = 500.f;
 	GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
 	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
