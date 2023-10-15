ba27793314d47a9aa61c3515750fb649c7a11247
Don-Smilo <don_smilo@mail.ru>
2023-10-15 14:58:04 +0300

    forgot to assign values of health
    :-)

--- Source/Platformer/PlatformerCharacter.cpp
+++ Source/Platformer/PlatformerCharacter.cpp
@@ -49,6 +49,11 @@ APlatformerCharacter::APlatformerCharacter()
 
 	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
 	// are set in the derived blueprint asset named ThirdPersonCharacter (to avoid direct content references in C++)
+
+
+	// Default health values
+	MaxHealth = 100.f;
+	Health = MaxHealth;
 }
 
 void APlatformerCharacter::BeginPlay()
